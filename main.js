362215f3657bcacb22f3bfdc329997b6cb12c18f
betoaleman1 <105089286+betoaleman1@users.noreply.github.com>
2022-11-07 19:56:52 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+song="";
+
+function preload(){
+    song=loadSound("music.mp3");
+}
+
+function setup(){
+    canvas=createCanvas(400,400);
+    canvas.position(400,200);
+    video=createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video,0,0,400,400);
+}
+
+function play(){
+    song.play();
+}
